48302aa55f6e42aff39c8c8e8606bbaec4ce57b1
tanno <94481731+tanno1@users.noreply.github.com>
2023-09-28 15:04:51 -0700

    started function, going home now...

--- Lab0x00.py
+++ Lab0x00.py
@@ -12,18 +12,25 @@ Lab 0x00 - Serial Communication and ADC Reading
 """
 
 # imports
-from pyb import Pin, Timer
+from pyb import Pin, Timer, ADC
 from array import array
 from time import sleep_ms
 
 
 # main
 
-# pin config
-    # configure PC1 as digital output
-    # configure PC0 as ADC
+# pin configuration
+PC1 = Pin(Pin.cpu.C1, mode=Pin.OUT_PP)
+PC0 = Pin(Pin.cpu.C0)
+ADC = ADC(PC0)
+    # to read... val = ADC.read()!
     
 # timer, trigger callbacks @ 1KHz w/ pyb.Timer class (timer 6, 7)
+
 def tim_cb(tim):
     global data, idx
     #f(n) code here!
+
+tim7 = Timer(7, freq = 1000)
+tim7.callback(tim_cb)
+    # to turn off callback -> tim7.callback(None)
