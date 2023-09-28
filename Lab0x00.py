e8c8c640ba0c8a4a69a3f176afadb581970c9f7b
tanno <94481731+tanno1@users.noreply.github.com>
2023-09-28 14:48:09 -0700

    Lab0x00 added

---
+++ Lab0x00.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 28 14:42:03 2023
+
+@author: Noah Tanner, Kozy Tonn
+
+ME405 - Refvem
+Lab 0x00 - Serial Communication and ADC Reading
+
+
+"""
+
+# imports
+
+# main
+
+# pin config
+    # configure PC1 as digital output
+    # configure PC0 as ADC
+    
+# timer, trigger callbacks @ 1KHz w/ pyb.Timer class
