6700fe11a59fbc0a039c88d7062689d9bb135721
tanno <94481731+tanno1@users.noreply.github.com>
2023-10-03 20:31:02 -0700

    working on main

--- HW1 - Mastermind/tanner_main.py
+++ HW1 - Mastermind/tanner_main.py
@@ -10,66 +10,103 @@ Fall 2023
 # definitions and initialization
 # set state into for state automatically
 state = 1
+win_amount = 0
+loss_amount = 0
+guess = 
 
 # print statements
-# start text for the beginning of the game
-start_text = """
-                            Welcome to the game of Mastermind!
-
-        You will be the codebreaker. Try to break the secret code by entering 4-digit
-        codes using the numbers 0-5. Your guesses will be marked with a (+) symbol for
-        each correct value in the correct location and a (-) for each correct value in
-        an incorrect location.
-
-                                    Press enter to begin
-        """
-# grid layout for the beginning of the game
 empty_grid = """
-+−−−+−−−+−−−+−−−+
-|   |   |   |   |
-+−−−+−−−+−−−+−−−+
-|   |   |   |   |
-+−−−+−−−+−−−+−−−+
-|   |   |   |   |
-+−−−+−−−+−−−+−−−+
-|   |   |   |   |
-+−−−+−−−+−−−+−−−+
-|   |   |   |   |
-+−−−+−−−+−−−+−−−+
-|   |   |   |   |
-+−−−+−−−+−−−+−−−+
-|   |   |   |   |
-+−−−+−−−+−−−+−−−+
-|   |   |   |   |
-+−−−+−−−+−−−+−−−+
-|   |   |   |   |
-+−−−+−−−+−−−+−−−+
-|   |   |   |   |
-+−−−+−−−+−−−+−−−+
-|   |   |   |   |
-+−−−+−−−+−−−+−−−+
-|   |   |   |   |
-+−−−+−−−+−−−+−−−+
-"""
+    +−−−+−−−+−−−+−−−+
+    |   |   |   |   |
+    +−−−+−−−+−−−+−−−+
+    |   |   |   |   |
+    +−−−+−−−+−−−+−−−+
+    |   |   |   |   |
+    +−−−+−−−+−−−+−−−+
+    |   |   |   |   |
+    +−−−+−−−+−−−+−−−+
+    |   |   |   |   |
+    +−−−+−−−+−−−+−−−+
+    |   |   |   |   |
+    +−−−+−−−+−−−+−−−+
+    |   |   |   |   |
+    +−−−+−−−+−−−+−−−+
+    |   |   |   |   |
+    +−−−+−−−+−−−+−−−+
+    |   |   |   |   |
+    +−−−+−−−+−−−+−−−+
+    |   |   |   |   |
+    +−−−+−−−+−−−+−−−+
+    |   |   |   |   |
+    +−−−+−−−+−−−+−−−+
+    |   |   |   |   |
+    +−−−+−−−+−−−+−−−+
+    """
+wins = "wins: "
+losses = "losses: "
+start_text = """                    Welcome to the game of Mastermind!
+
+            You will be the codebreaker. Try to break the secret code by entering 4-digit
+            codes using the numbers 0-5. Your guesses will be marked with a (+) symbol for
+            each correct value in the correct location and a (-) for each correct value in
+            an incorrect location.
+
+                                        Press enter to begin
+            """
+new_game = "Mastermind! Try to break the code. \n"
+        
+# grid layout for the beginning of the game
 
 while True:
     if state == 1:
         # state 1 - setup
-        # this state will print the starting game message, and await for an 'enter' value in order to move onto the next state and start the game
-
+        # print starting text 
         print(start_text)
+        
+        while True:
+            enter = input('')
+            if not enter:
+                break
 
+        state = 2                           # set next state
 
     # state 2 - guess
+    if state == 2:
+        # prints
+        print(new_game)
+        print(wins + ' ' + str(win_amount) + '\n')
+        print(losses + ' ' + str(loss_amount) + '\n')
+        print(empty_grid)
+
+        while True: 
+            enter = input('Enter a guess: ')
+            for n in enter: 
+                if n not in [0, 1, 2, 3, 4, 5]:
+                    print('Invalid guess...')
+                    True
+                elif n in [0, 1, 2, 3, 4, 5] and len(enter) == 4:
+                    False
 
     # state 3 - invalid guess
+    if state == 3:
+        pass
 
-    # state 4 - correct value 
+    # state 4 - correct value
+    if state == 4:
+        pass
 
     # state 5 - wrong value
+    if state == 5:
+        pass
 
     # state 6 - misplaced value
+    if state == 6:
+        pass
 
     # state 7 - win
+    if state == 7:
+        pass
 
     # state 8 - lose
+    if state == 8:
+        pass
