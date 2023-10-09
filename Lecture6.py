5862c5e5c909c6089af7c2eac38528798c9e0bf3
tanno <94481731+tanno1@users.noreply.github.com>
2023-10-09 11:58:30 -0700

    lecture over?...

--- Lecture6.py
+++ Lecture6.py
@@ -2,3 +2,24 @@
 # noah tanner
 # fall 2023
 
+'''
+doc strings, used to document speciric entities, files, class def, func def, attributes, etc...
+'''
+
+class dog:
+    '''!@brief (brief description of class) - dog class implementation
+        @details (details of class) - dog class
+    '''
+
+    def __init__(self, dogName, dogWeight, dogAge)
+        ''' !@brief
+            @details
+            @param dogName - [ description of dogName param ]
+            @param dogWeight - [ description of dogWeight param ]
+            @param dogAge - [ description of dogAge param ]
+        '''
+
+        # if we want to keep access to these class attributes, must store them by copying input param to attributes
+        self.name = dogName
+        self.weight = dogWeight
+        self.age = dogAge
