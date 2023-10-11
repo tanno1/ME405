00a7c396db56b104285fabf51e972fe76345e8dd
tanno <94481731+tanno1@users.noreply.github.com>
2023-10-11 12:35:40 -0700

    hw0x01 started

---
+++ HW0x01 - Reading CSV/main.py
@@ -0,0 +1,8 @@
+"""
+@file                                   main.py
+@brief                                  this is the main script for my python application for hw0x01
+@section dependencies Dependencies
+                                        This script relies on the following python libraries:
+                                            - 'matplotlib': for plotting data from the csv file
+
+"""
