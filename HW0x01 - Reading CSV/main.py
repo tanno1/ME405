703bcc952dc5d3c444cf110f3f5c4774af0c1fc3
tanno <94481731+tanno1@users.noreply.github.com>
2023-10-11 13:30:04 -0700

    finished the hw?... that was quick...

--- HW0x01 - Reading CSV/main.py
+++ HW0x01 - Reading CSV/main.py
@@ -1,8 +1,52 @@
 """
 @file                                   main.py
-@brief                                  this is the main script for my python application for hw0x01
+@author                                 noah tanner
+@assignment                             hw0x01 - reading csv files and using matplotlib
+@brief                                  this is the main script for my python application for hw0x01, this script will plot data from a csv file on a graph and includes proper error handling to accomodate different data points and whitespace or other errors that may be encountered in a csv file
 @section dependencies Dependencies
                                         This script relies on the following python libraries:
                                             - 'matplotlib': for plotting data from the csv file
-
 """
+
+# imports
+from matplotlib import pyplot as plt
+import csv
+
+if __name__ == '__main__':
+
+    # list for valid data to plot
+    x_data = []
+    y_data = []
+
+    with open('data.csv', 'r') as file:
+        # read csv file
+        csv_read = csv.reader(file)
+        # take header row and add as axis labels
+        header_row = next(csv_read)
+        xlabel = header_row[0]
+        ylabel = header_row[1]
+        
+        # iterate through rows and add data to x or y data list
+        for row in csv_read:
+            # check through only non-empty row
+            if not row:
+                continue
+            try:
+                x_data.append(float(row[0].split('#')[0].strip()))
+                y_data.append(float(row[1].split('#')[0].strip()))
+            except ( ValueError, IndexError ):
+                continue
+    
+    # create and plot data
+    plt.plot(x_data, y_data)
+
+    # add x and y axis labels
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+
+    # display plot
+    plt.show()
+
+
+
+
