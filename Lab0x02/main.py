e05ea1c06119799038cc7ccacd6bcde04baa85d9
tanno <94481731+tanno1@users.noreply.github.com>
2023-10-16 21:33:59 -0700

    current file, think we almost done finally

--- Lab0x02/main.py
+++ Lab0x02/main.py
@@ -23,7 +23,7 @@ class collector:
         self.tim            = tim
         self.motor          = motor
         self.encoder        = encoder
-        self.position = array( 'L', [0 for n in range(1000)]) 
+        self.position       = array( 'L', [0 for n in range(1000)]) 
         self.time           = array( 'L', [0 for n in range(1000)]) 
         self.delta          = array( 'L', [0 for n in range(1000)]) 
         self.idx            = 0
@@ -43,17 +43,19 @@ class collector:
             @details
         '''
         # add total position, time, and delta values to respective arrays
-        self.start_time               = time.ticks_us()                     # record current start time 
         self.encoder.update()                                               # update encoder position
-        self.position[self.idx]       = self.encoder.get_position()
-        self.delta[self.idx]          = self.encoder.get_delta()
-        self.end_time                 = time.ticks_us()                     # record end time
-        self.time[self.idx]           = self.start_time - self.end_time     # calculate the elapsed time
-        # increment array index 
+        self.position[self.idx]       = self.encoder.total_position
+        self.delta[self.idx]          = self.encoder.current_delta
+        self.time[self.idx]           = self.idx
         self.idx += 1
         if self.idx == 999:
             self.tim.callback(None)
             self.motor.disable()
+
+    def organize_data(self):
+        print('Time', 'Position', 'Delta')
+        for i in range(1000):
+            print(f"{self.time[i]},{self.position[i]},{self.delta[i]}")
 
 class Encoder:
     '''!@brief                  interface with quadrature encoders
@@ -69,22 +71,17 @@ class Encoder:
         self.chb = chb
         self.ar = ar
         self.ps = ps
-        self.prev_delta = 0             # initialize previous delta
         self.current_delta = 0          # initialize delta as 0 for first pass
         self.total_position = 0         # initialize total position as 0 for first pass
         self.prev_position = 0          # initialize previous position as 0 for first pass
         self.current_position = 0       # initialize the current position as 0 for first pass
 
         # to prevent MemoryException errors for repeat calculations:
-        self.under_check = ( self.ar / 2 )
-        self.over_check = ( -( self.ar + 1 )/2 )
+        self.under_check = ((self.ar+1)/2)
+        self.over_check = (-1*( self.ar + 1 ))/2
         self.ar_add_1 = self.ar + 1
 
     def update(self):
-        '''!@brief              updates encoder position and delta
-            @details
-            @param return
-        '''
         self.current_position = self.timer.counter()
         self.current_delta = self.current_position - self.prev_position
 
@@ -99,30 +96,16 @@ class Encoder:
         # add delta to total position (total movement that does not reset for each rev)
         self.total_position += self.current_delta
 
-        # update delta to check each time
-        self.prev_delta = self.current_delta
-
         # update previous position to current position
         self.prev_position = self.current_position
 
     def get_position(self):
-        '''!@brief              gets the most recent encoder position
-            @details
-            @return
-        '''
         return self.current_position
 
     def get_delta(self):
-        '''!@brief              gets the most recent encoder delta
-            @details
-            @return
-        '''
         return self.current_delta
 
     def zero(self):
-        '''!@brief              resets the encoder position to zero
-            @details
-        '''
         self.prev_delta         = 0             # initialize previous delta
         self.current_delta      = 0             # initialize delta as 0 for first pass
         self.total_position     = 0             # initialize total position as 0 for first pass
@@ -135,12 +118,12 @@ if __name__ == "__main__":
 
     # encoder 1 configuration 
     ps = 0
-    ar = 65535
-    ch_a_pin = Pin(Pin.cpu.B6, mode=Pin.OUT_PP)
-    ch_b_pin = Pin(Pin.cpu.B7, mode=Pin.OUT_PP)
-    tim_4 = Timer(4, period = ar, prescaler = ps)
-    cha = tim_4.channel(1, pin=ch_a_pin, mode=Timer.ENC_AB) #Timer.ENC_AB configures timer in encoder mode, counter changes when ch1 OR ch2 changes
-    chb = tim_4.channel(2, pin=ch_b_pin, mode=Timer.ENC_AB)
+    ar = 1000
+    ch_a_pin = Pin(Pin.cpu.C7, mode=Pin.OUT_PP)
+    ch_b_pin = Pin(Pin.cpu.C6, mode=Pin.OUT_PP)
+    tim_8 = Timer(8, period = ar, prescaler = ps)
+    cha = tim_8.channel(1, pin=ch_a_pin, mode=Timer.ENC_AB) #Timer.ENC_AB configures timer in encoder mode, counter changes when ch1 OR ch2 changes
+    chb = tim_8.channel(2, pin=ch_b_pin, mode=Timer.ENC_AB)
 
     # motor 1 configuration
 
@@ -153,7 +136,7 @@ if __name__ == "__main__":
     mot_A = mot_class.L6206(mot_tim_A, mot_EN1, mot_IN1, mot_IN2)
 
     # make encoder instance
-    encoder_1 = Encoder(tim_4, cha, chb, ar, ps)
+    encoder_1 = Encoder(tim_8, cha, chb, ar, ps)
     # make collector instance, assign callback method
     tim_6 = Timer(6, freq = 1000)
     collector_1 = collector(tim_6, encoder_1, mot_A)
@@ -161,12 +144,12 @@ if __name__ == "__main__":
     ### encoder & motor 2 setup ###
 
     # encoder 2 configuration
-    ch_a_pin_m2 = Pin(Pin.cpu.C7, mode=Pin.OUT_PP)
-    ch_b_pin_m2 = Pin(Pin.cpu.C6, mode=Pin.OUT_PP)
+    ch_a_pin_m2 = Pin(Pin.cpu.B6, mode=Pin.OUT_PP)
+    ch_b_pin_m2 = Pin(Pin.cpu.B7, mode=Pin.OUT_PP)
     # timer
-    tim_8 = Timer(8, period = ar, prescaler = ps)
-    cha_mot_2 = tim_8.channel(1, pin=ch_a_pin_m2, mode=Timer.ENC_AB)      # Timer.ENC_AB configures timer in encoder mode, counter changes when ch1 OR ch2 changes
-    chb_mot_2 = tim_8.channel(2, pin=ch_b_pin_m2, mode=Timer.ENC_AB)
+    tim_4 = Timer(4, period = ar, prescaler = ps)
+    cha_mot_2 = tim_4.channel(1, pin=ch_a_pin_m2, mode=Timer.ENC_AB)      # Timer.ENC_AB configures timer in encoder mode, counter changes when ch1 OR ch2 changes
+    chb_mot_2 = tim_4.channel(2, pin=ch_b_pin_m2, mode=Timer.ENC_AB)
 
     # motor 2 configuration
 
@@ -179,7 +162,7 @@ if __name__ == "__main__":
     mot_B = mot_class.L6206(mot_tim_B, EN2, INB_1, INB_2)
 
     # make encoder instance
-    encoder_2 = Encoder(tim_8, cha_mot_2, chb_mot_2, ar, ps)
+    encoder_2 = Encoder(tim_4, cha_mot_2, chb_mot_2, ar, ps)
     # make collector instance, assign callback method
     tim_7 = Timer(7, freq = 1000)
     collector_2 = collector(tim_7, encoder_2, mot_B)
