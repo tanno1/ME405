de97d3c5ac1f600dd62aec9e72177943cc76523a
tanno <94481731+tanno1@users.noreply.github.com>
2023-10-18 11:48:59 -0700

    rearranged folders/added lab0x03 instruction pdf

--- lecture_notes/lecture7pt2.py
+++ lecture_notes/lecture7pt2.py
@@ -1,32 +1,40 @@
-def taskGenFun():
+import time
 
+S0_INIT = 0
+S1_RUN = 1
+S2_RUN_THRICE = 2
+
+def taskGenFun():
     state = 0
     count = 0
+    yield state
 
     # run forever!
     while True:
-        if state == 0:
+        if state == S0_INIT:
             # run state 0 code
             print(f"state = {state}")
-            state = 1
+            state = S1_RUN
 
-        elif state == 1:
+        elif state == S1_RUN:
             # run state 1 code
             print(f"state = {state}")
-            state = 2
+            state = S2_RUN_THRICE
 
-        elif state == 2:
+        elif state == S2_RUN_THRICE:
             # run state 2 code
             print(f"state = {state}")
-            if count == 2:
-                state = 1
+            if (count == 2):
+                state = S1_RUN
                 count = 0
 
+            else:
+                count += 1
+    
         else:
-            count += 1
             raise ValueError("Invalid State")
 
-        yield state
+        yield(state)
 
 # create task obj as generator object
 task1 = taskGenFun()
