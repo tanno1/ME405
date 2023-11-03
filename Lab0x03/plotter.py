ce204d71d08fc1a1fa573722277bf93b5ddf78fc
tanno <94481731+tanno1@users.noreply.github.com>
2023-11-03 14:09:36 -0700

    still getting weird drop outs in the results, but professor said its alright, not sure what is causing it.

--- Lab0x03/plotter.py
+++ Lab0x03/plotter.py
@@ -7,10 +7,12 @@ Created on Fri Oct 20 12:56:15 2023
 
 from matplotlib import pyplot
 import serial
+import time
+import math
 
 pos_values = []
 time_values = []
-delta_values = []
+vel_values = []
 
 header1 = "Time (ms)"
 header2 = "Encoder Position (Ticks)"
@@ -25,7 +27,7 @@ def PosPlot():
     pyplot.show()
     
 def VeloPlot():
-    pyplot.plot(time_values, delta_values)
+    pyplot.plot(time_values, vel_values)
     pyplot.xlabel(header1)
     pyplot.ylabel(header3)
     pyplot.title(header3 +" Vs. "+ header1)
@@ -34,32 +36,49 @@ def VeloPlot():
 
 data = serial.Serial('/dev/cu.usbmodem142303', baudrate= 115600)
 
+def data_collect():
+    while True:
+        try:
+            line = data.readline().decode('utf-8', errors = 'replace').strip().split(',')
+            split = str(line[0])
+            split = split.split('\t')
+            print(split)
+            try: 
+                pos = float(split[0])
+                time = float(split[1])
+                vel = float(split[2]) * 3.66
+                pos_values.append(pos)
+                time_values.append(time)
+                vel_values.append(vel)
+            except IndexError:
+                print('Unicode Decode Error caused Indexing error, data point skipped')
+        
+        except UnicodeDecodeError:
+            print('Unicode Decode Error')
+
+        except ValueError:
+            print('Value')
+
+        except KeyboardInterrupt:            # exit collection
+            print('Keyboard interrupt')
+            PosPlot()                        # plot pos vs t
+            VeloPlot()                       # plot vel vs t
+
+def reset():
+    pos_values      = []
+    time_values     = []
+    vel_values    = []
+
 while True:
-    try:
-        line = data.readline().decode('utf-8', errors = 'replace').strip().split(',')
-        split = str(line[0])
-        split = split.split('\t')
-        print(split)
-        try: 
-            pos = float(split[0])
-            time = float(split[1])
-            delta = float(split[2])
-            pos_values.append(pos)
-            time_values.append(time)
-            delta_values.append(delta)
-        except IndexError:
-            print('Unicode Decode Error caused Indexing error, data point skipped')
-            continue
-    
-    except UnicodeDecodeError:
-        print('Unicode Decode Error')
-        continue
+    print("Command choices:")
+    print("1. data_collect()")
+    print("2. reset()")
+    time.sleep(2)
+    choice = input("Enter a command number: ")
 
-    except ValueError:
-        print('Value')
-        continue
-
-    except KeyboardInterrupt:            # exit collection
-        print('Keyboard interrupt')
-        PosPlot()                        # plot pos vs t
-        VeloPlot()                       # plot vel vs t
+    if choice == "1":
+        data_collect()
+    elif choice == "2":
+        reset()
+    else:
+        print("Invalid command, try again.")
