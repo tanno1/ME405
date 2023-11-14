7bb4e14ac5a78b3625547ab1dee202b408eb3fcd
tanno <94481731+tanno1@users.noreply.github.com>
2023-11-13 22:47:05 -0800

    romi driver class added

---
+++ Lab0x04/romi_driver.py
@@ -0,0 +1,17 @@
+'''
+    @name               romi_driver.py
+    @brief              new encoder class that must be used for the romi drivers
+    @description        TBD
+    @author             tanner, noah
+    @date               november, 2023
+'''
+
+class romi_driver:
+
+    def __init__(self):
+        '''
+            @name           init
+            @brief          initialized 
+        '''
+        pass
+
